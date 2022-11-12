3f5e92f18e2ae4ae037cd123ed598857acdb44bc
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2022-11-12 22:05:35 +0700

    rev button and spacing for gui,

--- src/gui.py
+++ src/gui.py
@@ -4,19 +4,19 @@ from tkinter.ttk import *
 from PIL import ImageTk, Image
 import os
 
-
-
 def create_test_image_frame(container):
 
     frame = ttk.Frame(container)
 
-    labelTestImage = ttk.Label(frame, text="Test Image")
+    labelTestImage = ttk.Label(frame, text="Test Image", style="Subheading02.TLabel")
     labelTestImage.grid(row=0, sticky="W")
 
+    # File dan folder dari test image
     file = 'Adriana Lima0_0.jpg'
     folder = 'test'
+    # Open dan show image
     dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join(folder, file))
-    img = Image.open(dir).resize((448, 448),Image.ANTIALIAS)
+    img = Image.open(dir).resize((400, 400),Image.LANCZOS)
     resizedImg = ImageTk.PhotoImage(img)
     labelImg = ttk.Label(frame, image=resizedImg)
     labelImg.grid(row=1)
@@ -28,13 +28,15 @@ def create_result_image_frame(container):
 
     frame = ttk.Frame(container)
 
-    labelTestImage = ttk.Label(frame, text="Closest Result")
+    labelTestImage = ttk.Label(frame, text="Closest Result", style="Subheading02.TLabel")
     labelTestImage.grid(row=0, sticky="W")
 
+    # File dan folder result image
     file = 'Adriana Lima0_0.jpg'
     folder = 'test'
+    # Open dan show image
     dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join(folder, file))
-    img = Image.open(dir).resize((448, 448),Image.ANTIALIAS)
+    img = Image.open(dir).resize((400, 400),Image.LANCZOS)
     resizedImg = ImageTk.PhotoImage(img)
     labelImg = ttk.Label(frame, image=resizedImg)
     labelImg.grid(row=1)
@@ -42,6 +44,84 @@ def create_result_image_frame(container):
 
     return frame
 
+# def chooseFolder():
+
+def create_choose_folder_button(container):
+    dir_choose_folder_inactive = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'button_choose_folder_inactive.png'))
+    choose_folder_inactive = ImageTk.PhotoImage(Image.open(dir_choose_folder_inactive))
+    dir_choose_folder_active = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'button_choose_folder_active.png'))
+    choose_folder_active = ImageTk.PhotoImage(Image.open(dir_choose_folder_active))
+
+    def on_enter(event):
+        button_choose_folder.config(image=choose_folder_active)
+        button_choose_folder.image = choose_folder_active
+
+    def on_leave(enter):
+        button_choose_folder.config(image=choose_folder_inactive)
+        button_choose_folder.image = choose_folder_inactive
+
+    # Masukkin chooseFolder as command di button ini
+    button_choose_folder = ttk.Button(container, image=choose_folder_inactive)
+    button_choose_folder.image = choose_folder_inactive
+
+    button_choose_folder.bind("<Enter>", on_enter)
+    button_choose_folder.bind("<Leave>", on_leave)
+
+    return button_choose_folder
+
+# def chooseFile():
+
+def create_choose_file_button(container):
+    dir_choose_file_inactive = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'button_choose_file_inactive.png'))
+    choose_file_inactive = ImageTk.PhotoImage(Image.open(dir_choose_file_inactive))
+    dir_choose_file_active = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'button_choose_file_active.png'))
+    choose_file_active = ImageTk.PhotoImage(Image.open(dir_choose_file_active))
+
+    def on_enter(event):
+        button_choose_file.config(image=choose_file_active)
+        button_choose_file.image = choose_file_active
+
+    def on_leave(enter):
+        button_choose_file.config(image=choose_file_inactive)
+        button_choose_file.image = choose_file_inactive
+
+    # Masukkin chooseFile as command di button ini
+    button_choose_file = ttk.Button(container, image=choose_file_inactive)
+    button_choose_file.image = choose_file_inactive
+
+    button_choose_file.bind("<Enter>", on_enter)
+    button_choose_file.bind("<Leave>", on_leave)
+
+    return button_choose_file
+
+# def openCamera():
+
+def create_open_camera_button(container):
+    dir_open_camera_inactive = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'button_open_camera_inactive.png'))
+    open_camera_inactive = ImageTk.PhotoImage(Image.open(dir_open_camera_inactive))
+    dir_open_camera_active = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'button_open_camera_active.png'))
+    open_camera_active = ImageTk.PhotoImage(Image.open(dir_open_camera_active))
+
+    def on_enter(event):
+        button_open_camera.config(image=open_camera_active)
+        button_open_camera.image = open_camera_active
+
+    def on_leave(enter):
+        button_open_camera.config(image=open_camera_inactive)
+        button_open_camera.image = open_camera_inactive
+
+    # Masukkin openCamera as command di button ini
+    button_open_camera = ttk.Button(container, image=open_camera_inactive)
+    button_open_camera.image = open_camera_inactive
+
+    button_open_camera.bind("<Enter>", on_enter)
+    button_open_camera.bind("<Leave>", on_leave)
+
+    return button_open_camera
+
+# def resultName():
+
+# def countExecutionTime():
 
 def create_info_frame(container):
 
@@ -51,86 +131,97 @@ def create_info_frame(container):
     frame.columnconfigure(0, weight=1)
     frame.columnconfigure(1, weight=2)
 
-    frame.rowconfigure(0, weight=2)
-    frame.rowconfigure(1, weight=2)
-    frame.rowconfigure(2, weight=1)
-    frame.rowconfigure(3, weight=2)
-    frame.rowconfigure(4, weight=2)
-    frame.rowconfigure(5, weight=1)
-    frame.rowconfigure(6, weight=2)
-    frame.rowconfigure(7, weight=2)
-    frame.rowconfigure(8, weight=1)
-    frame.rowconfigure(9, weight=2)
-
-    # def chooseFolder():
-
-    # def chooseFile():
-        
+    # Insert Dataset
     insertDataset = ttk.Label(frame, text="Insert Your Dataset", style='Subheading.TLabel')
     insertDataset.grid(column=0, row=0, columnspan=2, sticky='W')
-    chooseFolder = ttk.Button(frame, text="Choose Folder", command=frame.destroy)
-    chooseFolder.grid(column=0)
+    # Choose Folder Button
+    choose_folder_button = create_choose_folder_button(frame)
+    choose_folder_button.grid(column=0, row=1)
+    # Folder Name Chosen
     folderName = ttk.Label(frame, text="No file chosen")
     folderName.grid(column=1, row=1)
 
+    # Insert Test Image
     insertTestImage = ttk.Label(frame, text="Insert Your Test Image", style='Subheading.TLabel')
-    insertTestImage.grid(column=0, row=3, columnspan=2, sticky='W')
-    chooseFile = ttk.Button(frame, text="Choose File", command=frame.destroy)
-    chooseFile.grid(column=0, row=4, sticky='W')
+    insertTestImage.grid(column=0, row=2, columnspan=2, sticky='W', pady=(24, 0))
+    # Choose File Button
+    choose_file_button = create_choose_file_button(frame)
+    choose_file_button.grid(column=0, row=3, sticky='W')
+    # File Name Chosen
     fileName = ttk.Label(frame, text="No file chosen")
-    fileName.grid(column=1, row=4)
+    fileName.grid(column=1, row=3)
 
+    # or divider
+    dir_or = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'or_divider.png'))
+    img_or_divider = ImageTk.PhotoImage(Image.open(dir_or))
+    orDivider = ttk.Label(frame, image=img_or_divider)
+    orDivider.grid(column = 0, columnspan=2, row=4)
+    orDivider.image = img_or_divider
+
+    # Open Camera Button
+    open_camera_button = create_open_camera_button(frame)
+    open_camera_button.grid(columnspan=2, row=5, sticky='W')
+
+    # Result
     result = ttk.Label(frame, text="Result", style='Subheading.TLabel')
-    result.grid(column=0, row=6, columnspan=2,sticky='W')
+    result.grid(column=0, row=6, columnspan=2,sticky='W', pady=(24, 0))
     resultName = ttk.Label(frame, text="None")
     resultName.grid(column=0, row=7,columnspan=2,sticky='W')
 
+    # Execution Time
     executionTime = ttk.Label(frame, text="Execution time: 01:00")
-    executionTime.grid(column=0, row=9, columnspan=2,sticky='W')
+    executionTime.grid(column=0, row=8, columnspan=2,sticky='W', pady=(24, 0))
 
     return frame
 
 
 def create_main_window():
     root = tk.Tk()
-    root.title('recognice')
-    root.minsize(1280, 720)
+    root.title('recognice - Face Recognition')
+    root.minsize(1440, 900)
     root.config(bg = '#FFFFFF')
+    dirLogo = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets','logo.ico'))
+    root.iconbitmap(dirLogo)
 
-    # Layout on the root window
-    root.rowconfigure(0, weight=1)
-    root.rowconfigure(1, weight=2)
-    root.rowconfigure(2, weight=6)
+    # Create main frame
+    mainFrame = ttk.Frame(root)
+    mainFrame.place(relx=.5, rely=.47, anchor="c")
 
-    root.columnconfigure(0, weight=4)
-    root.columnconfigure(1, weight=4)
-    root.columnconfigure(2, weight=4)
+    # Layout on the main frame
+    mainFrame.rowconfigure(0, weight=1)
+    mainFrame.rowconfigure(1, weight=3)
 
     # logo
+    dir_logo_full = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'logo_full.png'))
+    logo_full = ImageTk.PhotoImage(Image.open(dir_logo_full))
+    logo_full_container = ttk.Label(root, image=logo_full)
+    logo_full_container.image = logo_full
+    logo_full_container.pack(side=tk.TOP, anchor=tk.NW, padx=60, pady=40)
 
     # title
-    ttk.Label(root, text="Recognize Face using Eigenface Method", style="Heading1.TLabel").grid(row=1,column=0,columnspan=3)
+    ttk.Label(mainFrame, text="Recognize Face using Eigenface Method", style="Heading1.TLabel").grid(row=0,column=0,columnspan=3, pady=(0, 60))
 
     # test image
-    test_image_frame = create_test_image_frame(root)
-    test_image_frame.grid(column=0, row=2)
+    test_image_frame = create_test_image_frame(mainFrame)
+    test_image_frame.grid(column=0, row=2, sticky='N')
 
     # result
-    result_image_frame = create_result_image_frame(root)
-    result_image_frame.grid(column=1, row=2)
+    result_image_frame = create_result_image_frame(mainFrame)
+    result_image_frame.grid(column=1, row=2, padx=80, sticky='N')
 
     # info 
-    info_frame = create_info_frame(root)
+    info_frame = create_info_frame(mainFrame)
     info_frame.grid(column=2,row=2)
 
     # style
     s = ttk.Style()
     s.theme_use('alt')
-    s.configure('.', font=('Dubai'))
-    s.configure('Heading1.TLabel', font=('Dubai 32 bold'), foreground='#6C56C2')
-    s.configure('Subheading.TLabel', font=('Dubai', 20), foreground='#6C56C2')
-    s.configure('TButton', background = '#FFD04D', foreground='#FFFFFF', relief='flat')
-    s.map('TButton', background=[('active','#FEBF10')], relief='flat')
+    s.configure('.', font=("Futura Bk BT", 20), foreground="#2F2D2D")
+    s.configure('Heading1.TLabel', font=("Futura Md BT", 36), foreground='#6C56C2')
+    s.configure('Subheading.TLabel', font=("Futura Md BT", 28), foreground='#6C56C2')
+    s.configure('Subheading02.TLabel', font=("Futura Md BT", 24), foreground='#2F2D2D')
+    s.configure('TButton', background = '#FFFFFF', foreground='#FFFFFF', relief='flat')
+    s.map('TButton', relief='flat', background=[('active','#FFFFFF')])
     s.configure('TFrame', background = '#FFFFFF')
     s.configure('TLabel', background = '#FFFFFF')
 
