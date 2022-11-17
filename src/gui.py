189a1655ec005252677cd4e5c8217a988cd2a23e
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2022-11-18 03:55:40 +0700

    add closest results, background img

--- src/gui.py
+++ src/gui.py
@@ -14,16 +14,55 @@ def create_test_image_frame(container):
     labelTestImage.grid(row=0, sticky="W")
 
     # File dan folder dari test image
-    file = 'Adriana Lima0_0.jpg'
-    folder = 'test'
+    file = 'no_image01.png'
+    folder = 'assets'
     # Open dan show image
     dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join(folder, file))
-    img = Image.open(dir).resize((400, 400),Image.LANCZOS)
+    img = Image.open(dir).resize((360, 360),Image.Resampling.LANCZOS)
     resizedImg = ImageTk.PhotoImage(img)
     labelImg = ttk.Label(frame, image=resizedImg)
     labelImg.grid(row=1)
     labelImg.image = resizedImg
     return frame
+
+def create_other_closest_result(container):
+    frame = ttk.Frame(container)
+
+    labelTestImage = ttk.Label(frame, text="Other Closest Results", style="Subheading02.TLabel")
+    labelTestImage.grid(row=0, sticky="W", columnspan=3)
+
+    # File dan folder closest result 1
+    file = 'no_image02.png'
+    folder = 'assets'
+    # Open dan show image closest result 1
+    dir01 = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join(folder, file))
+    img01 = ImageTk.PhotoImage(Image.open(dir01))
+    labelImg = ttk.Label(frame, image=img01)
+    labelImg.grid(row=1, column=0, sticky='W')
+    labelImg.image = img01
+
+    # File dan folder closest result 2
+    file = 'no_image02.png'
+    folder = 'assets'
+    # Open dan show image closest result 1
+    dir02 = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join(folder, file))
+    img02 = ImageTk.PhotoImage(Image.open(dir02))
+    labelImg = ttk.Label(frame, image=img02)
+    labelImg.grid(row=1, column=1)
+    labelImg.image = img02
+
+    # File dan folder closest result 3
+    file = 'no_image02.png'
+    folder = 'assets'
+    # Open dan show image closest result 1
+    dir03 = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join(folder, file))
+    img03 = ImageTk.PhotoImage(Image.open(dir03))
+    labelImg = ttk.Label(frame, image=img03)
+    labelImg.grid(row=1, column=2)
+    labelImg.image = img03
+
+    return frame
+    
 
 def create_result_image_frame(container):
 
@@ -33,15 +72,19 @@ def create_result_image_frame(container):
     labelTestImage.grid(row=0, sticky="W")
 
     # File dan folder result image
-    file = 'Adriana Lima0_0.jpg'
-    folder = 'test'
+    file = 'no_image01.png'
+    folder = 'assets'
     # Open dan show image
     dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join(folder, file))
-    img = Image.open(dir).resize((400, 400),Image.LANCZOS)
+    img = Image.open(dir).resize((360, 360),Image.Resampling.LANCZOS)
     resizedImg = ImageTk.PhotoImage(img)
     labelImg = ttk.Label(frame, image=resizedImg)
-    labelImg.grid(row=1)
+    labelImg.grid(row=1, sticky='W')
     labelImg.image = resizedImg
+
+    # Other closest result
+    other_closest_result = create_other_closest_result(frame)
+    other_closest_result.grid(row=2, pady=(8, 0))
 
     return frame
 
@@ -171,12 +214,18 @@ def create_info_frame(container):
     # Result
     result = ttk.Label(frame, text="Result", style='Subheading.TLabel')
     result.grid(column=0, row=6, columnspan=2,sticky='W', pady=(24, 0))
-    resultName = ttk.Label(frame, text="None")
-    resultName.grid(column=0, row=7,columnspan=2,sticky='W')
+    resultName01 = ttk.Label(frame, text="None")
+    resultName01.grid(column=0, row=7,columnspan=2,sticky='W')
+    resultName02 = ttk.Label(frame, text="None")
+    resultName02.grid(column=0, row=8,columnspan=2,sticky='W')
+    resultName03 = ttk.Label(frame, text="None")
+    resultName03.grid(column=0, row=9,columnspan=2,sticky='W')
+    resultName04 = ttk.Label(frame, text="None")
+    resultName04.grid(column=0, row=10,columnspan=2,sticky='W')
 
     # Execution Time
     executionTime = ttk.Label(frame, text="Execution time: 01:00")
-    executionTime.grid(column=0, row=8, columnspan=2,sticky='W', pady=(24, 0))
+    executionTime.grid(column=0, row=11, columnspan=2,sticky='W', pady=(24, 0))
 
     return frame
 
@@ -189,13 +238,20 @@ def create_main_window():
     dirLogo = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets','logo.ico'))
     root.iconbitmap(dirLogo)
 
+    # background
+    dir_background = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'background.png'))
+    background = ImageTk.PhotoImage(Image.open(dir_background))
+    background_container = ttk.Label(root, image=background)
+    background_container.image = background
+    background_container.place(rely=1, relx=0.5, anchor='s')
+
     # Create main frame
     mainFrame = ttk.Frame(root)
     mainFrame.place(relx=.5, rely=.47, anchor="c")
 
     # Layout on the main frame
     mainFrame.rowconfigure(0, weight=1)
-    mainFrame.rowconfigure(1, weight=3)
+    mainFrame.rowconfigure(1, weight=4)
 
     # logo
     dir_logo_full = os.path.join(os.path.dirname(os.path.dirname(__file__)), os.path.join('assets', 'logo_full.png'))
@@ -205,7 +261,7 @@ def create_main_window():
     logo_full_container.pack(side=tk.TOP, anchor=tk.NW, padx=60, pady=40)
 
     # title
-    ttk.Label(mainFrame, text="Recognize Face using Eigenface Method", style="Heading1.TLabel").grid(row=0,column=0,columnspan=3, pady=(0, 60))
+    ttk.Label(mainFrame, text="Recognize Face using Eigenface Method", style="Heading1.TLabel").grid(row=0,column=0,columnspan=3, pady=(20, 40))
 
     # test image
     test_image_frame = create_test_image_frame(mainFrame)
@@ -213,7 +269,7 @@ def create_main_window():
 
     # result
     result_image_frame = create_result_image_frame(mainFrame)
-    result_image_frame.grid(column=1, row=2, padx=80, sticky='N')
+    result_image_frame.grid(column=1, row=2, sticky='N', padx=50)
 
     # info 
     info_frame = create_info_frame(mainFrame)
@@ -222,10 +278,10 @@ def create_main_window():
     # style
     s = ttk.Style()
     s.theme_use('alt')
-    s.configure('.', font=("Futura Bk BT", 20), foreground="#2F2D2D")
+    s.configure('.', font=("Futura Bk BT", 18), foreground="#2F2D2D")
     s.configure('Heading1.TLabel', font=("Futura Md BT", 36), foreground='#6C56C2')
-    s.configure('Subheading.TLabel', font=("Futura Md BT", 28), foreground='#6C56C2')
-    s.configure('Subheading02.TLabel', font=("Futura Md BT", 24), foreground='#2F2D2D')
+    s.configure('Subheading.TLabel', font=("Futura Md BT", 24), foreground='#6C56C2')
+    s.configure('Subheading02.TLabel', font=("Futura Md BT", 20), foreground='#2F2D2D')
     s.configure('TButton', background = '#FFFFFF', foreground='#FFFFFF', relief='flat')
     s.map('TButton', relief='flat', background=[('active','#FFFFFF')])
     s.configure('TFrame', background = '#FFFFFF')
